3e292094408477365d6e3d567844dc8baea8c9b7
Erxhan <kamberierxhan@gmail.com>
2022-05-16 20:57:04 -0400

    Created VPC and EC2 modules

---
+++ CHILD/EC2/variable.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+  default = "ami-0c02fb55956c7d316"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+# variable "subnet_id" {
+# }
+
+
+variable "server_tag" {
+  default = "app-server"
+}
